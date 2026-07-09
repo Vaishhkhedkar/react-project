f17db9aea7be0dedae4ce6b1a75c7d8203b48d83
Vaishnavi vitthal Khedkar <166608890+Vaishhkhedkar@users.noreply.github.com>
2026-07-09 13:47:47 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+// import Dashboard from "./layouts/Dashboard";
+
+// function App() {
+//   return <Dashboard />;
+// }
+
+// export default App;
+
+
+import Dashboard from "./layouts/Dashboard";
+
+function App() {
+  return <Dashboard />;
+}
+
+export default App;
